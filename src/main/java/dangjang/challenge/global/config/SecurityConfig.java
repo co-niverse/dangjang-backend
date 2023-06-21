ce5938ce1330d4b94599bd21689f07a13fb705c4
NAM GIBEOM <rlqja0523@naver.com>
2023-06-22 00:43:12 +0900

    feat: security 설정 (request permission 수정 필요)

---
+++ src/main/java/dangjang/challenge/global/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package dangjang.challenge.global.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * Spring Security를 설정한다.
+ *
+ * @author Teo
+ * @since 1.0
+ */
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.headers(httpSecurityHeadersConfigurer -> httpSecurityHeadersConfigurer.frameOptions(
+				HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(corsConfigurer -> corsConfigurer.configurationSource(configurationSource()))
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(
+				sessionManagementConfigurer -> sessionManagementConfigurer.sessionCreationPolicy(
+					SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource configurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("*"));
+		configuration.setAllowedMethods(List.of("*"));
+		configuration.setAllowedHeaders(List.of("*", "Authorization"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
