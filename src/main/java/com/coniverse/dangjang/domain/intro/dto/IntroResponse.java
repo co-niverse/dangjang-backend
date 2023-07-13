8bc3f71e7491e4d61ae36897857239f252026d7e
NAM GIBEOM <rlqja0523@naver.com>
2023-07-13 23:28:36 +0900

    docs: javadoc 수정

--- src/main/java/com/coniverse/dangjang/domain/intro/dto/IntroResponse.java
+++ src/main/java/com/coniverse/dangjang/domain/intro/dto/IntroResponse.java
@@ -5,11 +5,10 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
- * 버전 정보, splash 정보를 담는다.
+ * splash screen time때 load할 버전 정보와 데이터를 담는 dto이다.
  *
  * @author TEO
  * @since 1.0.0
  */
-public record IntroResponse<T>(String minVersion, String latestVersion,
-							   @JsonInclude(JsonInclude.Include.NON_NULL) List<T> home) {
+public record IntroResponse<T>(String minVersion, String latestVersion, @JsonInclude(JsonInclude.Include.NON_NULL) List<T> loadData) {
 }
