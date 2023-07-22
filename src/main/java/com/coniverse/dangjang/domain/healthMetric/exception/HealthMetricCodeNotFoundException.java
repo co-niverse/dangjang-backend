4c2b404c8c9f63c2893a0bc6aaddf47afb984ec2
NAM GIBEOM <rlqja0523@naver.com>
2023-07-22 14:43:52 +0900

    docs: 클래스 설명 추가

--- src/main/java/com/coniverse/dangjang/domain/healthMetric/exception/HealthMetricCodeNotFoundException.java
+++ src/main/java/com/coniverse/dangjang/domain/healthMetric/exception/HealthMetricCodeNotFoundException.java
@@ -3,6 +3,8 @@ package com.coniverse.dangjang.domain.healthMetric.exception;
 import com.coniverse.dangjang.global.exception.BusinessException;
 
 /**
+ * 건강지표 코드를 찾을 수 없을 때 발생하는 예외
+ *
  * @author TEO
  * @since 1.0.0
  */
