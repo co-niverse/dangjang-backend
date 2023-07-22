6b3a3d24cc4dfe73c6cbcd5d60bfdc8f252ef2b8
NAM GIBEOM <rlqja0523@naver.com>
2023-07-22 14:01:40 +0900

    feat: validation 추가

--- src/main/java/com/coniverse/dangjang/domain/healthMetric/controller/HealthMetricController.java
+++ src/main/java/com/coniverse/dangjang/domain/healthMetric/controller/HealthMetricController.java
@@ -1,6 +1,7 @@
 package com.coniverse.dangjang.domain.healthMetric.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -9,12 +10,16 @@ import com.coniverse.dangjang.domain.healthMetric.dto.response.HealthMetricRespo
 import com.coniverse.dangjang.global.dto.SuccessMultiResponse;
 import com.coniverse.dangjang.global.dto.SuccessSingleResponse;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+
 /**
  * 건강 지표 controller interface이다.
  *
  * @author TEO
  * @since 1.0.0
  */
+@Validated
 public interface HealthMetricController<RQ extends HealthMetricRequest, RP extends HealthMetricResponse> {
 	/**
 	 * HTTP POST METHOD
@@ -22,21 +27,23 @@ public interface HealthMetricController<RQ extends HealthMetricRequest, RP exten
 	 *
 	 * @since 1.0.0
 	 */
-	ResponseEntity<SuccessSingleResponse<RP>> post(@PathVariable(value = "month") int month, @PathVariable(name = "day") int day, @RequestBody RQ requestBody);
+	ResponseEntity<SuccessSingleResponse<RP>> post(@Positive @PathVariable(value = "month") int month, @Positive @PathVariable(name = "day") int day,
+		@Valid @RequestBody RQ requestBody);
 
 	/**
 	 * HTTP PATCH METHOD
 	 *
 	 * @since 1.0.0
 	 */
-	ResponseEntity<SuccessSingleResponse<RP>> patch(@PathVariable(value = "month") int month, @PathVariable(name = "day") int day, @RequestBody RQ requestBody);
+	ResponseEntity<SuccessSingleResponse<RP>> patch(@Positive @PathVariable(value = "month") int month, @Positive @PathVariable(name = "day") int day,
+		@Valid @RequestBody RQ requestBody);
 
 	/**
 	 * HTTP GET METHOD
 	 *
 	 * @since 1.0.0
 	 */
-	ResponseEntity<SuccessMultiResponse<RP>> get(@PathVariable(value = "month") int month, @PathVariable(name = "day") int day);
+	ResponseEntity<SuccessMultiResponse<RP>> get(@Positive @PathVariable(value = "month") int month, @Positive @PathVariable(name = "day") int day);
 
 }
 
