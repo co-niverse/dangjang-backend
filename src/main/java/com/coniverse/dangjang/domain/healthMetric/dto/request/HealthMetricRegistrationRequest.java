8728c78105ece9d4d4966e403ebb000909cd4c70
NAM GIBEOM <rlqja0523@naver.com>
2023-07-23 03:53:53 +0900

    refactor: unit 타입 수정

--- src/main/java/com/coniverse/dangjang/domain/healthMetric/dto/request/HealthMetricRegistrationRequest.java
+++ src/main/java/com/coniverse/dangjang/domain/healthMetric/dto/request/HealthMetricRegistrationRequest.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 
 /**
  * 건강지표 request dto abstract class이다.
@@ -12,11 +11,7 @@ import jakarta.validation.constraints.PositiveOrZero;
  * @author TEO
  * @since 1.0.0
  */
-public record HealthMetricRegistrationRequest(@NotBlank(message = "건강지표 타입은 null이거나 공백일 수 없습니다.")
-											  String healthMetricType,
-											  @NotNull(message = "건강지표 생성일은 null일 수 없습니다.")
-											  LocalDate createdAt,
-											  @PositiveOrZero(message = "단위는 0보다 작을 수 없습니다.")
-											  @NotNull(message = "단위는 null일 수 없습니다.")
-											  Integer unit) {
+public record HealthMetricRegistrationRequest(@NotBlank(message = "건강지표 타입은 필수로 입력해야 합니다.") String healthMetricType,
+											  @NotNull(message = "건강지표 등록일은 필수로 입력해야 합니다.") LocalDate createdAt,
+											  @NotBlank(message = "단위는 필수로 입력해야 합니다.") String unit) {
 }
