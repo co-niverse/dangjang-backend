101e8c69aa125b3116081fae0896b8201e834259
13wjdgk <13wjdgk@naver.com>
2023-07-24 15:57:04 +0900

    test : SignUpServiceTest 추가

---
+++ src/test/java/com/coniverse/dangjang/domain/user/service/UserServiceTest.java
@@ -0,0 +1,41 @@
+package com.coniverse.dangjang.domain.user.service;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.coniverse.dangjang.domain.auth.dto.Response.LoginResponse;
+import com.coniverse.dangjang.domain.user.dto.SignUpRequest;
+import com.coniverse.dangjang.domain.user.dto.TestDto;
+
+@SpringBootTest
+public class UserServiceTest {
+	@Autowired
+	private UserService userService;
+
+	@Test()
+	void 새로운_유저를_추가한다_카카오() {
+		//given
+		SignUpRequest signUpRequest = TestDto.getSignUpRequest();
+
+		//when
+		LoginResponse loginResponse = userService.signUp(signUpRequest);
+		//that
+		assertThat(loginResponse).isNotNull();
+	}
+
+	@Test()
+	void 새로운_유저를_추가한다_네이버() {
+		//given
+		SignUpRequest signUpRequest = TestDto.getSignUpRequest();
+		signUpRequest.setActivityAmount("MEDIUM");
+		signUpRequest.setProvider("naver");
+		//when
+		LoginResponse loginResponse = userService.signUp(signUpRequest);
+		//that
+		assertThat(loginResponse).isNotNull();
+	}
+
+}
