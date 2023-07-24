80c49764d1fd8e31da314e3bfba5ade1332a31d1
NAM GIBEOM <rlqja0523@naver.com>
2023-07-24 15:42:11 +0900

    feat: 생성일자가 올바르지 않을 때 예외 발생

--- src/main/java/com/coniverse/dangjang/domain/healthMetric/util/CreatedAtUtil.java
+++ src/main/java/com/coniverse/dangjang/domain/healthMetric/util/CreatedAtUtil.java
@@ -1,8 +1,11 @@
 package com.coniverse.dangjang.domain.healthMetric.util;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 
 import org.springframework.stereotype.Component;
+
+import com.coniverse.dangjang.domain.healthMetric.exception.IncorrectCreatedAtException;
 
 /**
  * 생성일자와 관련한 util
@@ -18,9 +21,14 @@ public class CreatedAtUtil {
 	 * @param month 생성일자의 월
 	 * @param day   생성일자의 일
 	 * @return LocalDate 생성일자
+	 * @throws IncorrectCreatedAtException 생성일자가 올바르지 않을 때 발생하는 예외
 	 * @since 1.0.0
 	 */
 	public LocalDate generateCreatedAt(int month, int day) {
-		return LocalDate.of(LocalDate.now().getYear(), month, day);
+		try {
+			return LocalDate.of(LocalDate.now().getYear(), month, day);
+		} catch (DateTimeException e) {
+			throw new IncorrectCreatedAtException();
+		}
 	}
 }
