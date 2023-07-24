e348eb9d3998b18912c98307e331e5d6ca6d520e
NAM GIBEOM <rlqja0523@naver.com>
2023-07-24 19:59:03 +0900

    docs: JavaDoc @param 추가

--- src/main/java/com/coniverse/dangjang/domain/healthMetric/service/HealthMetricRegistrationService.java
+++ src/main/java/com/coniverse/dangjang/domain/healthMetric/service/HealthMetricRegistrationService.java
@@ -17,6 +17,8 @@ public interface HealthMetricRegistrationService {
 	/**
 	 * 건강지표를 저장한다.
 	 *
+	 * @param request   건강지표 request post dto
+	 * @param createdAt 건강지표 생성일
 	 * @since 1.0.0
 	 */
 	HealthMetricResponse save(HealthMetricPostRequest request, LocalDate createdAt);
@@ -24,6 +26,8 @@ public interface HealthMetricRegistrationService {
 	/**
 	 * 건강지표를 수정한다.
 	 *
+	 * @param request   건강지표 request patch dto
+	 * @param createdAt 건강지표 생성일
 	 * @since 1.0.0
 	 */
 	HealthMetricResponse update(HealthMetricPatchRequest request, LocalDate createdAt);
