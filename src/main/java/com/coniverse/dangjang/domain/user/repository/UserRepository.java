e5b108825ac73f0163d0d094f7b4fcdf4692c1bf
NAM GIBEOM <rlqja0523@naver.com>
2023-07-28 02:46:54 +0900

    fix: userId 복합키 제거

--- src/main/java/com/coniverse/dangjang/domain/user/repository/UserRepository.java
+++ src/main/java/com/coniverse/dangjang/domain/user/repository/UserRepository.java
@@ -8,15 +8,7 @@ import com.coniverse.dangjang.domain.user.entity.User;
  * USER Repository
  *
  * @author EVE
- * @since 1.0
+ * @since 1.0.0
  */
-// public interface UserRepository extends JpaRepository<User, UserId> {
 public interface UserRepository extends JpaRepository<User, String> {
-	/**
-	 * OauthID(카카오,네이버 사용자 ID)로 사용자 정보 조회
-	 *
-	 * @since 1.0
-	 */
-	// @Query("SELECT u FROM User u WHERE u.userId.oauthId = ?1 AND u.userId.oauthProvider = ?2")
-	// Optional<User> findByUserId(String oauthId, OauthProvider oauthProvider);
 }
