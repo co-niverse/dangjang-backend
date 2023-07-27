e1f741b58beca9435cc85909984aa04200fc04ed
13wjdgk <13wjdgk@naver.com>
2023-07-28 01:45:20 +0900

    feat : 닉네임 중복확인 validation 추가

--- src/main/java/com/coniverse/dangjang/domain/user/controller/UserController.java
+++ src/main/java/com/coniverse/dangjang/domain/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.coniverse.dangjang.domain.user.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,8 @@ import com.coniverse.dangjang.domain.user.dto.DuplicateNicknameResponse;
 import com.coniverse.dangjang.domain.user.service.UserService;
 import com.coniverse.dangjang.global.dto.SuccessSingleResponse;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -19,7 +22,8 @@ import lombok.RequiredArgsConstructor;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/duplicateNickname")
+@RequestMapping("/api")
+@Validated
 public class UserController {
 	private final UserService userService;
 
@@ -28,8 +32,9 @@ public class UserController {
 	 * @return 닉네임이 중복되지 않았으면 true, 중복된 닉네임이면 false를 담은 DuplicateNicknameResponse 객체를 반환한다.
 	 * @since 1.0
 	 */
-	@GetMapping("")
-	public ResponseEntity<SuccessSingleResponse<DuplicateNicknameResponse>> checkDuplicateNickname(@RequestParam String nickname) {
+	@GetMapping("/duplicateNickname")
+	public ResponseEntity<SuccessSingleResponse<DuplicateNicknameResponse>> checkDuplicateNickname(
+		@RequestParam @Pattern(regexp = "^[a-zA-Z]*${1,8}", message = "닉네임은 1~8글자 이내여야 합니다.") @NotBlank(message = "닉네임은 1~8글자 이내여야 합니다.") String nickname) {
 		DuplicateNicknameResponse duplicateNicknameResponse = userService.checkDublicateNickname(nickname);
 		return ResponseEntity.ok().body(new SuccessSingleResponse<>(HttpStatus.OK.getReasonPhrase(), duplicateNicknameResponse));
 	}
