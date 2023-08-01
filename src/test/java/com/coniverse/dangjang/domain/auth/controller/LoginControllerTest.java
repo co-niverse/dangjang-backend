d125dbdd4682aa52bc2ba62f02b3f263d24b4d14
NAM GIBEOM <rlqja0523@naver.com>
2023-08-01 14:34:13 +0900

    fix: 테스트 수정

--- src/test/java/com/coniverse/dangjang/domain/auth/controller/LoginControllerTest.java
+++ src/test/java/com/coniverse/dangjang/domain/auth/controller/LoginControllerTest.java
@@ -1,48 +1,105 @@
 package com.coniverse.dangjang.domain.auth.controller;
 
+import static com.coniverse.dangjang.fixture.LoginFixture.*;
 import static com.coniverse.dangjang.support.SimpleMockMvc.*;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.coniverse.dangjang.domain.auth.dto.request.KakaoLoginRequest;
+import com.coniverse.dangjang.domain.auth.dto.request.NaverLoginRequest;
+import com.coniverse.dangjang.domain.auth.dto.response.LoginResponse;
+import com.coniverse.dangjang.domain.auth.service.OauthLoginService;
 import com.coniverse.dangjang.support.ControllerTest;
 
 /**
- * @author EVE
+ * @author EVE, TEO
  * @since 1.0.0
  */
 class LoginControllerTest extends ControllerTest {
-	private final String URI = "/api/auth";
+	private static final String URI = "/api/auth";
+	private final LoginResponse response = 로그인_응답();
+	@Autowired
+	private OauthLoginService oauthLoginService;
 
 	@Test
-	void 카카오_로그인_성공한다() throws Exception {
+	void 카카오_로그인에_성공한다() throws Exception {
+		// given
+		KakaoLoginRequest request = 카카오_로그인_요청();
+		String content = objectMapper.writeValueAsString(request);
+		given(oauthLoginService.login(request)).willReturn(response);
 
 		// when
-		ResultActions resultActions = post(mockMvc, URI + "/kakao",
-			"{\"accessToken\" : \"accessToken123456789\"}");
+		ResultActions resultActions = post(mockMvc, URI + "/kakao", content);
 
 		// then
 		resultActions.andExpectAll(
 			status().isOk(),
-			jsonPath("$.message").value(HttpStatus.OK.getReasonPhrase())
+			jsonPath("$.message").value(HttpStatus.OK.getReasonPhrase()),
+			jsonPath("$.data.oauthId").value(response.oauthId()),
+			jsonPath("$.data.accessToken").value(response.accessToken()),
+			jsonPath("$.data.refreshToken").value(response.refreshToken())
 		);
 
 	}
 
 	@Test
-	void 네이버_로그인_성공한다() throws Exception {
+	void 카카오_로그인_시_RequestBody의_access_token이_비어있으면_예외가_발생한다() throws Exception {
+		// given
+		KakaoLoginRequest request = new KakaoLoginRequest("");
+		String content = objectMapper.writeValueAsString(request);
 
 		// when
-		ResultActions resultActions = post(mockMvc, URI + "/naver",
-			"{\"accessToken\" : \"accessToken123456789\"}");
+		ResultActions resultActions = post(mockMvc, URI + "/kakao", content);
+
+		// then
+		resultActions.andExpectAll(
+			status().isBadRequest(),
+			jsonPath("$.errorCode").value(400),
+			jsonPath("$.fieldErrors[0].field").value("accessToken"),
+			jsonPath("$.fieldErrors[0].rejectedValue").value("")
+		);
+	}
+
+	@Test
+	void 네이버_로그인에_성공한다() throws Exception {
+		// given
+		NaverLoginRequest request = 네이버_로그인_요청();
+		String content = objectMapper.writeValueAsString(request);
+		given(oauthLoginService.login(request)).willReturn(response);
+
+		// when
+		ResultActions resultActions = post(mockMvc, URI + "/naver", content);
 
 		// then
 		resultActions.andExpectAll(
 			status().isOk(),
-			jsonPath("$.message").value(HttpStatus.OK.getReasonPhrase())
+			jsonPath("$.message").value(HttpStatus.OK.getReasonPhrase()),
+			jsonPath("$.data.oauthId").value(response.oauthId()),
+			jsonPath("$.data.accessToken").value(response.accessToken()),
+			jsonPath("$.data.refreshToken").value(response.refreshToken())
 		);
 	}
 
+	@Test
+	void 네이버_로그인_시_RequestBody의_access_token이_비어있으면_예외가_발생한다() throws Exception {
+		// given
+		NaverLoginRequest request = new NaverLoginRequest(" ");
+		String content = objectMapper.writeValueAsString(request);
+
+		// when
+		ResultActions resultActions = post(mockMvc, URI + "/naver", content);
+
+		// then
+		resultActions.andExpectAll(
+			status().isBadRequest(),
+			jsonPath("$.errorCode").value(400),
+			jsonPath("$.fieldErrors[0].field").value("accessToken"),
+			jsonPath("$.fieldErrors[0].rejectedValue").value(" ")
+		);
+	}
 }
--- src/test/java/com/coniverse/dangjang/domain/auth/service/OauthLoginServiceTest.java
+++ src/test/java/com/coniverse/dangjang/domain/auth/service/OauthLoginServiceTest.java
@@ -1,17 +1,19 @@
 package com.coniverse.dangjang.domain.auth.service;
 
+import static com.coniverse.dangjang.fixture.LoginFixture.*;
 import static com.coniverse.dangjang.fixture.UserFixture.*;
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.coniverse.dangjang.domain.auth.dto.request.KakaoLoginRequest;
 import com.coniverse.dangjang.domain.auth.dto.response.LoginResponse;
 import com.coniverse.dangjang.domain.infrastructure.auth.dto.KakaoInfoResponse;
+import com.coniverse.dangjang.domain.user.entity.User;
 import com.coniverse.dangjang.domain.user.exception.NonExistentUserException;
 import com.coniverse.dangjang.domain.user.repository.UserRepository;
 import com.coniverse.dangjang.domain.user.service.UserService;
@@ -21,74 +23,48 @@ import com.coniverse.dangjang.domain.user.service.UserService;
  * @since 1.0.0
  */
 @SpringBootTest
+@Transactional
 class OauthLoginServiceTest {
 	@Autowired
 	private OauthLoginService oauthLoginService;
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
-	private AuthTokenGenerator authTokenGenerator;
-	@Autowired
 	private UserService userService;
-	private static final String accessToken = "12345678";
 
 	@Test
-	@WithAnonymousUser
-	void 로그인_실패한다() throws NonExistentUserException {
-
+	void 가입된_유저가_아니면_로그인을_실패한다() {
 		//given
-		KakaoLoginRequest kakaoLoginParams = new KakaoLoginRequest(accessToken);
+		KakaoLoginRequest request = 카카오_로그인_요청();
 
-		//then
-		Assertions.assertThrows(NonExistentUserException.class, () -> {
-			oauthLoginService.login(kakaoLoginParams);
-		});
-
+		// when & then
+		assertThatThrownBy(() -> oauthLoginService.login(request))
+			.isInstanceOf(NonExistentUserException.class);
 	}
 
 	@Test
-	void 로그인을_성공한다() {
+	void 가입된_유저면_로그인을_성공한다() {
 		//given
-		KakaoLoginRequest kakaoLoginParams = new KakaoLoginRequest(accessToken);
-		userRepository.save(유저_가은());
+		User 이브 = userRepository.save(유저_이브());
+		KakaoLoginRequest request = 카카오_로그인_요청();
 
 		//when
-		LoginResponse actual = oauthLoginService.login(kakaoLoginParams);
+		LoginResponse response = oauthLoginService.login(request);
+
 		//then
-		assertThat(actual).isNotNull();
+		assertAll(
+			() -> assertThat(response.oauthId()).isEqualTo(이브.getOauthId()),
+			() -> assertThat(response.nickname()).isEqualTo(이브.getNickname()),
+			() -> assertThat(response.accessToken()).isNotNull(),
+			() -> assertThat(response.refreshToken()).isNotNull()
+		);
 	}
 
-	// @Test
-	// void JWT_반환한다() throws NonExistentUserException {
-	// 	// given
-	// 	KakaoInfoResponse kakaoInfoResponse = new KakaoInfoResponse("12345678");
-	// 	userRepository.save(유저_가은());
-	//
-	// 	// when
-	// 	User findUser = userService.findUserByOauthId(kakaoInfoResponse.getOauthId());
-	//
-	// 	// then
-	// 	if (findUser != null) {
-	// 		AuthToken authToken = authTokenGenerator.generate(findUser.getOauthId());
-	// 		assertThat(authToken).isNotNull();
-	// 	}
-	// }
-
 	@Test
-	void 새로운_유저를_추가한다() {
+	void 새로운_유저를_추가한다() { // TODO userService test로 빼기
 		//given
 		KakaoInfoResponse kakaoInfoResponse = new KakaoInfoResponse("12345678");
 		String userId = userService.signUp(kakaoInfoResponse);
 		assertThat(userId).isNotNull();
 	}
-
-	// @Test
-	// void 존재하지_않는_유저_여부_확인하기() throws NonExistentUserException {
-	// 	//given
-	// 	KakaoInfoResponse kakaoInfoResponse = new KakaoInfoResponse("12345678");
-	//
-	// 	Assertions.assertThrows(NonExistentUserException.class, () -> {
-	// 		userService.findUserByOauthId(kakaoInfoResponse.getOauthId());
-	// 	});
-	// }
 }
