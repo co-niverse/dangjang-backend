487af0dfdecc6ea714b4d863fc1650fd6ab134c2
NAM GIBEOM <rlqja0523@naver.com>
2023-08-01 13:25:49 +0900

    fix: 스키마 변경에 따라 data 변경

--- src/main/resources/data-local.sql
+++ src/main/resources/data-local.sql
@@ -1,7 +1,9 @@
-INSERT INTO users (oauth_id, oauth_provider, nickname, gender, birthday, activity_amount, height, recommended_calorie, status, profile_image_path, created_at,
+INSERT INTO users (oauth_id, oauth_provider, nickname, gender, birthday, activity_amount, height, recommended_calorie, status, role, profile_image_path,
+                   created_at,
                    updated_at)
-VALUES ('11111111', 'KAKAO', 'TEO', 'M', '1997-05-23', 'MEDIUM', 180, 2000, 'ACTIVE', 'user/image/teo', now(), now());
+VALUES ('11111111', 'KAKAO', 'TEO', 'M', '1997-05-23', 'MEDIUM', 180, 2000, 'ACTIVE', 'ADMIN', 'user/image/teo', now(), now());
 
-INSERT INTO users (oauth_id, oauth_provider, nickname, gender, birthday, activity_amount, height, recommended_calorie, status, profile_image_path, created_at,
+INSERT INTO users (oauth_id, oauth_provider, nickname, gender, birthday, activity_amount, height, recommended_calorie, status, role, profile_image_path,
+                   created_at,
                    updated_at)
-VALUES ('22222222', 'NAVER', 'EVE', 'F', '2001-12-24', 'LOW', 160, 1500, 'ACTIVE', 'user/image/eve', now(), now());
+VALUES ('22222222', 'NAVER', 'EVE', 'F', '2001-12-24', 'LOW', 160, 1500, 'ACTIVE', 'USER', 'user/image/eve', now(), now());
