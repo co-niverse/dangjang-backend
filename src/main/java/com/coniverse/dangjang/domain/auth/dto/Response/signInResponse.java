07357eb4ec506aff50cb8fb2926d8079f28df69a
13wjdgk <13wjdgk@naver.com>
2023-08-02 17:45:34 +0900

    merge : signInResponse 제거

--- src/main/java/com/coniverse/dangjang/domain/auth/dto/Response/signInResponse.java
+++
@@ -1,5 +0,0 @@
-package com.coniverse.dangjang.domain.auth.dto.response;
-
-public record signInResponse(String accessToken, String refreshToken, String nickname, boolean dangjangClub, boolean healthConnect) {
-
-}
