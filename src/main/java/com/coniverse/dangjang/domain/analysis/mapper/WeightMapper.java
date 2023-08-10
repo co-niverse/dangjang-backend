28fc18dce6146e6aba1bbd685dd5abc8af53651a
13wjdgk <13wjdgk@naver.com>
2023-08-10 17:51:07 +0900

    feat : weightmapper 추가

---
+++ src/main/java/com/coniverse/dangjang/domain/analysis/mapper/WeightMapper.java
@@ -0,0 +1,21 @@
+package com.coniverse.dangjang.domain.analysis.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import com.coniverse.dangjang.domain.analysis.document.WeightFeedback;
+import com.coniverse.dangjang.domain.analysis.vo.AnalysisData.WeightAnalysisData;
+import com.coniverse.dangjang.global.util.EnumFindUtil;
+
+@Mapper(componentModel = "spring", imports = {EnumFindUtil.class})
+public interface WeightMapper {
+	@Mappings({
+		@Mapping(target = "oauthId", source = "weightAnalysisData.oauthId"),
+		@Mapping(target = "createdAt", source = "weightAnalysisData.createdAt"),
+		@Mapping(target = "weightDiff", source = "weightAnalysisData.weightDiff"),
+		@Mapping(target = "weightSteps", source = "weightAnalysisData.weightSteps"),
+		@Mapping(target = "feedback", source = "feedback")
+	})
+	WeightFeedback toDocument(WeightAnalysisData weightAnalysisData, String feedback);
+}
