795985017f9906c14196588498b72188edd4a706
13wjdgk <13wjdgk@naver.com>
2023-08-11 18:14:28 +0900

    test : HealthMetricTest 체중 테스트 수정

--- src/test/java/com/coniverse/dangjang/domain/feedback/WeightFeedbackServiceTest.java
+++ src/test/java/com/coniverse/dangjang/domain/feedback/WeightFeedbackServiceTest.java
@@ -3,54 +3,37 @@ package com.coniverse.dangjang.domain.feedback;
 import static com.coniverse.dangjang.fixture.HealthMetricFixture.*;
 import static com.coniverse.dangjang.fixture.UserFixture.*;
 import static org.assertj.core.api.AssertionsForClassTypes.*;
-import static org.junit.jupiter.params.provider.Arguments.*;
 
 import java.time.LocalDate;
-import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.coniverse.dangjang.domain.analysis.document.WeightFeedback;
+import com.coniverse.dangjang.domain.analysis.vo.AnalysisData.WeightAnalysisData;
 import com.coniverse.dangjang.domain.code.enums.CommonCode;
+import com.coniverse.dangjang.domain.feedback.document.WeightFeedback;
 import com.coniverse.dangjang.domain.feedback.repository.WeightFeedbackRepository;
-import com.coniverse.dangjang.domain.healthmetric.dto.request.HealthMetricPostRequest;
-import com.coniverse.dangjang.domain.healthmetric.dto.response.HealthMetricResponse;
-import com.coniverse.dangjang.domain.healthmetric.entity.HealthMetric;
-import com.coniverse.dangjang.domain.healthmetric.repository.HealthMetricRepository;
-import com.coniverse.dangjang.domain.healthmetric.service.HealthMetricRegistrationService;
+import com.coniverse.dangjang.domain.feedback.service.WeightFeedbackService;
 import com.coniverse.dangjang.domain.user.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class WeightFeedbackServiceTest {
 	private final LocalDate 등록_일자 = LocalDate.of(2023, 12, 31);
 	@Autowired
-	private HealthMetricRegistrationService healthMetricRegistrationService;
-	@Autowired
-	private HealthMetricRepository healthMetricRepository;
+	private WeightFeedbackService weightFeedbackService;
 	@Autowired
 	private WeightFeedbackRepository weightFeedbackRepository;
 	@Autowired
 	private UserRepository userRepository;
 
 	private String 테오_아이디;
-	private HealthMetric 등록된_건강지표;
-
-	static Stream<Arguments> 체중_건강지표_목록() {
-		return Stream.of(arguments(CommonCode.WT_MEM, "30"), arguments(CommonCode.WT_MEM, "50"), arguments(CommonCode.WT_MEM, "100"),
-			arguments(CommonCode.WT_MEM, "120"), arguments(CommonCode.WT_MEM, "150"));
-	}
 
 	@BeforeAll
 	void setUp() {
@@ -59,18 +42,15 @@ public class WeightFeedbackServiceTest {
 
 	@ParameterizedTest
 	@Transactional
-	@MethodSource("체중_건강지표_목록")
+	@MethodSource("com.coniverse.dangjang.fixture.HealthMetricFixture#체중_건강지표_목록")
 	void 체중별_조언을_성공적으로_등록한다(CommonCode commonCode, String unit) {
 		// given
-		HealthMetricPostRequest request = 건강지표_등록_요청(commonCode, unit);
+		WeightAnalysisData weightAnalysisData = 체중분석_데이터(commonCode, unit);
 		weightFeedbackRepository.deleteByFeedbackId(테오_아이디, 등록_일자);
-
 		// when
-		HealthMetricResponse response = healthMetricRegistrationService.register(request, 등록_일자, 테오_아이디);
-
+		weightFeedbackService.saveFeedback(weightAnalysisData);
 		// then
-		WeightFeedback 등록된_건강지표 = weightFeedbackRepository.findByFeedbackId(테오_아이디, response.createdAt());
-
+		WeightFeedback 등록된_건강지표 = weightFeedbackRepository.findByFeedbackId(테오_아이디, 등록_일자);
 		assertThat(등록된_건강지표.getFeedback()).isNotBlank();
 	}
 
--- src/test/java/com/coniverse/dangjang/fixture/HealthMetricFixture.java
+++ src/test/java/com/coniverse/dangjang/fixture/HealthMetricFixture.java
@@ -1,7 +1,14 @@
 package com.coniverse.dangjang.fixture;
 
-import java.time.LocalDate;
+import static com.coniverse.dangjang.fixture.UserFixture.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
 
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import com.coniverse.dangjang.domain.analysis.vo.AnalysisData.WeightAnalysisData;
 import com.coniverse.dangjang.domain.code.enums.CommonCode;
 import com.coniverse.dangjang.domain.healthmetric.dto.request.HealthMetricPatchRequest;
 import com.coniverse.dangjang.domain.healthmetric.dto.request.HealthMetricPostRequest;
@@ -22,9 +29,16 @@ public class HealthMetricFixture {
 	private static final CommonCode 등록_건강지표 = CommonCode.BS_BBF;
 	private static final String 등록_건강지표_상세명 = 등록_건강지표.getTitle();
 	private static final String 수정_건강지표_상세명 = CommonCode.BS_ALC.getTitle();
+	private static final String 체중_등록_건강지표_단위 = "50"; //TODO 건강지표 등록 요청 메서드 merge 되면 삭제하기
+	private static final String 체중_등록_건강지표_상세명 = "체중"; //TODO 건강지표 등록 요청 메서드 merge 되면 삭제하기
 
 	public static HealthMetricPostRequest 건강지표_등록_요청() {
 		return new HealthMetricPostRequest(등록_건강지표_상세명, 등록_건강지표_단위);
+	}
+
+	//TODO 건강지표 등록 요청 메서드 merge 되면 삭제하기
+	public static HealthMetricPostRequest 체중_건강지표_등록_요청() {
+		return new HealthMetricPostRequest(체중_등록_건강지표_상세명, 체중_등록_건강지표_단위);
 	}
 
 	public static HealthMetricPatchRequest 타입_변경한_건강지표_수정_요청() {
@@ -46,5 +60,24 @@ public class HealthMetricFixture {
 			.user(user)
 			.unit(등록_건강지표_단위)
 			.build();
+	}
+
+	public static HealthMetric 건강지표_더미데이터(User user, CommonCode commonCode, String unit) {
+		return HealthMetric.builder()
+			.createdAt(생성일자)
+			.commonCode(commonCode)
+			.user(user)
+			.unit(unit)
+			.build();
+	}
+
+	public static Stream<Arguments> 체중_건강지표_목록() {
+		return Stream.of(arguments(CommonCode.WT_MEM, "30"), arguments(CommonCode.WT_MEM, "50"), arguments(CommonCode.WT_MEM, "100"),
+			arguments(CommonCode.WT_MEM, "120"), arguments(CommonCode.WT_MEM, "150"));
+	}
+
+	public static WeightAnalysisData 체중분석_데이터(CommonCode commonCode, String unit) {
+		return new WeightAnalysisData(건강지표_더미데이터(유저_테오(), commonCode, unit), 유저_테오());
+
 	}
 }
