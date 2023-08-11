9528c136a6dcadda586d99379a986e66120b8186
13wjdgk <13wjdgk@naver.com>
2023-08-11 19:01:38 +0900

    refactor : WeightSteps enum 속성 대문자 변경

--- src/main/java/com/coniverse/dangjang/domain/analysis/enums/WeightSteps.java
+++ src/main/java/com/coniverse/dangjang/domain/analysis/enums/WeightSteps.java
@@ -7,12 +7,12 @@ package com.coniverse.dangjang.domain.analysis.enums;
  * @since 1.0.0
  */
 public enum WeightSteps {
-	underweight("저체중"),
-	normal("정상체중"),
-	overweight("과체중"),
-	obesity("경도비만"),
-	mildObesity("중등도비만"),
-	highObesity("고도비만");
+	UNDERWEIGHT("저체중"),
+	NORMAL("정상체중"),
+	OVERWEIGHT("과체중"),
+	OBESITY("경도비만"),
+	MILDOBESITY("중등도비만"),
+	HIGHOBESITY("고도비만");
 
 	private final String title;
 
@@ -26,17 +26,17 @@ public enum WeightSteps {
 
 	public static WeightSteps of(double bmi) {
 		if (bmi < 18.5) {
-			return underweight;
+			return UNDERWEIGHT;
 		} else if (bmi < 22.9) {
-			return normal;
+			return NORMAL;
 		} else if (bmi < 24.9) {
-			return overweight;
+			return OVERWEIGHT;
 		} else if (bmi < 29.9) {
-			return obesity;
+			return OBESITY;
 		} else if (bmi < 34.9) {
-			return mildObesity;
+			return MILDOBESITY;
 		} else {
-			return highObesity;
+			return HIGHOBESITY;
 		}
 	}
 }
