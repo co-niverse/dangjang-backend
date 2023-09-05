b214fea28b031283ecdcaea5ce084ad3d228f225
NAM GIBEOM <rlqja0523@naver.com>
2023-09-06 02:16:00 +0900

    feat: common code fixture 추가

--- src/test/java/com/coniverse/dangjang/domain/analysis/service/AnalysisServiceTest.java
+++ src/test/java/com/coniverse/dangjang/domain/analysis/service/AnalysisServiceTest.java
@@ -1,5 +1,6 @@
 package com.coniverse.dangjang.domain.analysis.service;
 
+import static com.coniverse.dangjang.fixture.CommonCodeFixture.*;
 import static org.mockito.Mockito.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import com.coniverse.dangjang.domain.analysis.strategy.AnalysisStrategy;
 import com.coniverse.dangjang.domain.analysis.strategy.BloodSugarAnalysisStrategy;
+import com.coniverse.dangjang.domain.analysis.strategy.WeightAnalysisStrategy;
 import com.coniverse.dangjang.domain.code.enums.CommonCode;
 import com.coniverse.dangjang.fixture.HealthMetricFixture;
 
@@ -29,28 +31,20 @@ class AnalysisServiceTest {
 	private AnalysisService analysisService;
 	@SpyBean
 	private BloodSugarAnalysisStrategy bloodSugarAnalysisStrategy;
-	private static final List<CommonCode> 혈당 = List.of(
-		CommonCode.EMPTY_STOMACH,
-		CommonCode.BEFORE_BREAKFAST,
-		CommonCode.AFTER_BREAKFAST,
-		CommonCode.BEFORE_LUNCH,
-		CommonCode.AFTER_LUNCH,
-		CommonCode.BEFORE_DINNER,
-		CommonCode.AFTER_DINNER,
-		CommonCode.BEFORE_SLEEP,
-		CommonCode.ETC
-	);
+	@SpyBean
+	private WeightAnalysisStrategy weightAnalysisStrategy;
 
-	private Stream<Arguments> provideHealthMetric() {
+	private Stream<Arguments> provideHealthMetricType() {
 		return Stream.of(
-			Arguments.of(혈당, bloodSugarAnalysisStrategy)
-			// TODO 운동, 체중, 당화혈색소, 식단 추가
+			Arguments.of(혈당_타입(), bloodSugarAnalysisStrategy),
+			Arguments.of(체중_타입(), weightAnalysisStrategy)
+			// TODO 운동, 당화혈색소, 식단 추가
 		);
 	}
 
 	@ParameterizedTest
-	@MethodSource("provideHealthMetric")
-	void 건강지표에_따라_알맞은_분석_전략을_선택하여_호출한다(List<CommonCode> type, AnalysisStrategy analysisStrategy) {
+	@MethodSource("provideHealthMetricType")
+	void 건강지표_타입에_따라_알맞은_분석_전략을_선택하여_호출한다(List<CommonCode> type, AnalysisStrategy analysisStrategy) {
 		// given & when
 		type.stream()
 			.map(HealthMetricFixture::건강지표_엔티티)
---
+++ src/test/java/com/coniverse/dangjang/fixture/CommonCodeFixture.java
@@ -0,0 +1,39 @@
+package com.coniverse.dangjang.fixture;
+
+import java.util.List;
+
+import com.coniverse.dangjang.domain.code.enums.CommonCode;
+
+public class CommonCodeFixture {
+	public static List<CommonCode> 혈당_타입() {
+		return List.of(
+			CommonCode.EMPTY_STOMACH,
+			CommonCode.BEFORE_BREAKFAST,
+			CommonCode.AFTER_BREAKFAST,
+			CommonCode.BEFORE_LUNCH,
+			CommonCode.AFTER_LUNCH,
+			CommonCode.BEFORE_DINNER,
+			CommonCode.AFTER_DINNER,
+			CommonCode.BEFORE_SLEEP,
+			CommonCode.ETC
+		);
+	}
+
+	public static List<CommonCode> 운동_타입() {
+		return List.of(
+			CommonCode.STEP_COUNT
+		);
+	}
+
+	public static List<CommonCode> 체중_타입() {
+		return List.of(
+			CommonCode.MEASUREMENT
+		);
+	}
+
+	public static List<CommonCode> 당화혈색소_타입() {
+		return List.of(
+			CommonCode.HBA1C
+		);
+	}
+}
