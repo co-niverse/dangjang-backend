8eb32a4d9df356a52e9830bc84e3688b3f246455
NAM GIBEOM <rlqja0523@naver.com>
2023-09-10 18:50:57 +0900

    refactor: 가이드 document interface 제거

--- src/main/java/com/coniverse/dangjang/domain/guide/weight/document/WeightGuide.java
+++ src/main/java/com/coniverse/dangjang/domain/guide/weight/document/WeightGuide.java
@@ -8,7 +8,6 @@ import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import com.coniverse.dangjang.domain.analysis.enums.Alert;
 import com.coniverse.dangjang.domain.code.enums.CommonCode;
-import com.coniverse.dangjang.domain.guide.common.document.Guide;
 
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
@@ -25,7 +24,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @Document
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class WeightGuide implements Guide {
+public class WeightGuide {
 	@Id
 	@Field(value = "_id", targetType = FieldType.OBJECT_ID)
 	private String id;
