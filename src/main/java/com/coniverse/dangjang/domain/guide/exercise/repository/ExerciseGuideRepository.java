e856c07b3d7135905f4eca451ca2de4dfb39af49
13wjdgk <13wjdgk@naver.com>
2023-09-12 02:10:16 +0900

    refactor : 운동 repository 수정

--- src/main/java/com/coniverse/dangjang/domain/guide/exercise/repository/ExerciseGuideRepository.java
+++ src/main/java/com/coniverse/dangjang/domain/guide/exercise/repository/ExerciseGuideRepository.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import com.coniverse.dangjang.domain.guide.exercise.document.ExerciseGuide;
 
@@ -23,6 +22,6 @@ public interface ExerciseGuideRepository extends MongoRepository<ExerciseGuide, 
 	 * @return 운동 가이드
 	 * @since 1.0.0
 	 */
-	@Query("{'oauthId': ?0, 'createdAt': ?1}")
+	
 	Optional<ExerciseGuide> findByOauthIdAndCreatedAt(String oauthId, LocalDate createdAt);
 }
