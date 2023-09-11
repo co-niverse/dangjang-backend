1b08d373b51aee1d408f22987bd0bad6c8485a28
13wjdgk <13wjdgk@naver.com>
2023-09-11 20:04:59 +0900

    refactore : ExerciseMapper 불필요한 Mapping 제거

--- src/main/java/com/coniverse/dangjang/domain/guide/exercise/mapper/ExerciseMapper.java
+++ src/main/java/com/coniverse/dangjang/domain/guide/exercise/mapper/ExerciseMapper.java
@@ -3,8 +3,6 @@ package com.coniverse.dangjang.domain.guide.exercise.mapper;
 import java.util.List;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 import com.coniverse.dangjang.domain.analysis.dto.healthMetric.ExerciseAnalysisData;
 import com.coniverse.dangjang.domain.guide.exercise.document.ExerciseGuide;
@@ -25,37 +23,24 @@ public interface ExerciseMapper {
 	 * 운동 가이드(걸음수) Document 생성
 	 *
 	 * @param exerciseAnalysisData 운동 분석 데이터
-	 * @param guide                운동 가이드 내용
+	 * @param content              운동 가이드 내용
 	 * @param comparedToLastWeek   저번주 대비 걸음수 가이드 내용
 	 * @return WeightGuide
 	 * @since 1.0.0
 	 */
-	@Mappings({
-		@Mapping(target = "oauthId", source = "exerciseAnalysisData.oauthId"),
-		@Mapping(target = "createdAt", source = "exerciseAnalysisData.createdAt"),
-		@Mapping(target = "needStepByTTS", source = "exerciseAnalysisData.needStepByTTS"),
-		@Mapping(target = "needStepByLastWeek", source = "exerciseAnalysisData.needStepByLastWeek"),
-		@Mapping(target = "content", source = "guide"),
-		@Mapping(target = "comparedToLastWeek", source = "comparedToLastWeek")
-	})
-	ExerciseGuide toDocument(ExerciseAnalysisData exerciseAnalysisData, String guide, String comparedToLastWeek);
+
+	ExerciseGuide toDocument(ExerciseAnalysisData exerciseAnalysisData, String content, String comparedToLastWeek);
 
 	/**
 	 * 운동 가이드(칼로리) Document 생성
 	 *
 	 * @param exerciseAnalysisData 운동 분석 데이터
-	 * @param exerciseCalorie      운동 칼로리
+	 * @param exerciseCalories     운동 칼로리
 	 * @return WeightGuide
 	 * @since 1.0.0
 	 */
-	@Mappings({
-		@Mapping(target = "oauthId", source = "exerciseAnalysisData.oauthId"),
-		@Mapping(target = "createdAt", source = "exerciseAnalysisData.createdAt"),
-		@Mapping(target = "needStepByTTS", source = "exerciseAnalysisData.needStepByTTS"),
-		@Mapping(target = "needStepByLastWeek", source = "exerciseAnalysisData.needStepByLastWeek"),
-		@Mapping(target = "exerciseCalories", source = "exerciseCalorie")
-	})
-	ExerciseGuide toDocument(ExerciseAnalysisData exerciseAnalysisData, List<ExerciseCalorie> exerciseCalorie);
+
+	ExerciseGuide toDocument(ExerciseAnalysisData exerciseAnalysisData, List<ExerciseCalorie> exerciseCalories);
 
 	/**
 	 * 운동 가이드 Document를  Response로 변경
