ff71be3daf8e168cb85dd0631013af0746f7ca34
NAM GIBEOM <rlqja0523@naver.com>
2023-09-13 01:14:28 +0900

    refactor: merge 후 오류 수정 및 리팩토링

---
+++ src/main/java/com/coniverse/dangjang/domain/analysis/enums/ExerciseCalorie.java
@@ -0,0 +1,39 @@
+package com.coniverse.dangjang.domain.analysis.enums;
+
+import java.util.Arrays;
+
+import com.coniverse.dangjang.domain.code.enums.CommonCode;
+import com.coniverse.dangjang.global.exception.EnumNonExistentException;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 운동 칼로리 percentage
+ *
+ * @author TEO
+ * @since 1.0.0
+ */
+@Getter
+@AllArgsConstructor
+public enum ExerciseCalorie {
+	WALK(0.9),
+	RUN(2),
+	BIKE(2.3),
+	SWIM(2),
+	HIKING(1.5),
+	HEALTH(1.5);
+
+	private final double percentage;
+
+	/**
+	 *
+	 */
+	public static double findPercentByExercise(CommonCode type) {
+		return Arrays.stream(ExerciseCalorie.values())
+			.filter(c -> c.name().equals(type.name()))
+			.findFirst()
+			.map(ExerciseCalorie::getPercentage)
+			.orElseThrow(EnumNonExistentException::new);
+	}
+}
