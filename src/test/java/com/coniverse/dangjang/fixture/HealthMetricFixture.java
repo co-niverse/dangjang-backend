d616e8856f206702423224ebc60f602c1aabfe0a
NAM GIBEOM <rlqja0523@naver.com>
2023-09-13 02:22:06 +0900

    test: 헬스 커넥트 등록 service test

--- src/test/java/com/coniverse/dangjang/fixture/HealthMetricFixture.java
+++ src/test/java/com/coniverse/dangjang/fixture/HealthMetricFixture.java
@@ -4,8 +4,11 @@ import static com.coniverse.dangjang.fixture.GuideFixture.*;
 import static com.coniverse.dangjang.fixture.UserFixture.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.stream.IntStream;
 
 import com.coniverse.dangjang.domain.code.enums.CommonCode;
+import com.coniverse.dangjang.domain.healthmetric.dto.request.HealthConnectPostRequest;
 import com.coniverse.dangjang.domain.healthmetric.dto.request.HealthMetricPatchRequest;
 import com.coniverse.dangjang.domain.healthmetric.dto.request.HealthMetricPostRequest;
 import com.coniverse.dangjang.domain.healthmetric.dto.response.HealthMetricResponse;
@@ -29,6 +32,14 @@ public class HealthMetricFixture {
 
 	public static HealthMetricPostRequest 건강지표_등록_요청() {
 		return new HealthMetricPostRequest(등록_건강지표_상세명, 생성일자, 등록_건강지표_단위);
+	}
+
+	public static HealthConnectPostRequest 헬스_커넥트_등록_요청(String type, int count) {
+		return new HealthConnectPostRequest(new HashSet<>(
+			IntStream.range(0, count)
+				.mapToObj(i -> new HealthMetricPostRequest(type, LocalDate.of(2023, 1, 1).plusDays(i).toString(), "140"))
+				.toList())
+		);
 	}
 
 	public static HealthMetricPatchRequest 타입_변경한_건강지표_수정_요청() {
