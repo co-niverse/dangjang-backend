04a06ddc3a58274966f856775338732681c54831
NAM GIBEOM <rlqja0523@naver.com>
2023-09-13 12:27:37 +0900

    docs: JavaDoc 추가

--- src/main/java/com/coniverse/dangjang/domain/auth/handler/AuthErrorUtil.java
+++ src/main/java/com/coniverse/dangjang/domain/auth/handler/AuthErrorUtil.java
@@ -6,7 +6,21 @@ import org.springframework.http.MediaType;
 
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * 인증 에러 응답을 처리하는 util
+ *
+ * @author TEO
+ * @since 1.0.0
+ */
 public class AuthErrorUtil {
+	/**
+	 * 인증 에러 응답을 전송한다.
+	 *
+	 * @param response      HttpServletResponse
+	 * @param status        응답 상태 코드
+	 * @param errorResponse 응답 본문
+	 * @since 1.0.0
+	 */
 	public static void sendErrorResponse(HttpServletResponse response, int status, String errorResponse) throws IOException {
 		response.setStatus(status);
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
