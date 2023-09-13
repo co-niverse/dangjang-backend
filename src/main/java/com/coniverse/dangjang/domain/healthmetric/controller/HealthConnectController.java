4302f85f600dd23f66435f962671dd17ae588852
NAM GIBEOM <rlqja0523@naver.com>
2023-09-13 22:25:51 +0900

    fix: HTTP Status 어노테이션 삭제

--- src/main/java/com/coniverse/dangjang/domain/healthmetric/controller/HealthConnectController.java
+++ src/main/java/com/coniverse/dangjang/domain/healthmetric/controller/HealthConnectController.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coniverse.dangjang.domain.healthmetric.dto.request.HealthConnectPostRequest;
@@ -37,7 +36,6 @@ public class HealthConnectController {
 	 * @since 1.0.0
 	 */
 	@PostMapping
-	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<SuccessSingleResponse<?>> postHealthConnect(@Valid @RequestBody HealthConnectPostRequest requests,
 		@AuthenticationPrincipal User principal) {
 		String oauthId = principal.getUsername();
