5e72695a2c88309afbc9ed3d7db81a87c6adfef4
NAM GIBEOM <rlqja0523@naver.com>
2023-09-21 00:10:57 +0900

    refactor: 디렉토리 이동

--- src/main/java/com/coniverse/dangjang/global/aop/advice/GlobalExceptionAdvice.java
+++ src/main/java/com/coniverse/dangjang/global/aop/advice/GlobalExceptionAdvice.java
@@ -1,4 +1,4 @@
-package com.coniverse.dangjang.global.advice;
+package com.coniverse.dangjang.global.aop.advice;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
