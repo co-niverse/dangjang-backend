8b6499b988ad635ec355b47ba9804c775ba60f14
NAM GIBEOM <rlqja0523@naver.com>
2023-10-01 04:37:12 +0900

    fix: @Profile 제거

--- src/main/java/com/coniverse/dangjang/global/aop/aspect/log/LogAspect.java
+++ src/main/java/com/coniverse/dangjang/global/aop/aspect/log/LogAspect.java
@@ -4,7 +4,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
@@ -24,7 +23,6 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Aspect
 @RequiredArgsConstructor
-@Profile({"!test", "!performance"})
 @Slf4j
 public class LogAspect {
 	private final RestTemplate restTemplate;
