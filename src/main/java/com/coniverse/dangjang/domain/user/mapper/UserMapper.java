7a871adc31208eea486a9813e7b097665626268e
NAM GIBEOM <rlqja0523@naver.com>
2023-10-09 04:46:02 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/coniverse/dangjang/domain/user/mapper/UserMapper.java
+++ src/main/java/com/coniverse/dangjang/domain/user/mapper/UserMapper.java
@@ -3,16 +3,13 @@ package com.coniverse.dangjang.domain.user.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import com.coniverse.dangjang.domain.healthmetric.enums.HealthConnect;
 import com.coniverse.dangjang.domain.infrastructure.auth.dto.OAuthInfoResponse;
 import com.coniverse.dangjang.domain.user.dto.request.SignUpRequest;
 import com.coniverse.dangjang.domain.user.entity.User;
 import com.coniverse.dangjang.domain.user.entity.enums.ActivityAmount;
 import com.coniverse.dangjang.domain.user.entity.enums.Gender;
-import com.coniverse.dangjang.domain.user.entity.enums.Role;
-import com.coniverse.dangjang.domain.user.entity.enums.Status;
 
-@Mapper(componentModel = "spring", imports = {Role.class, Status.class, HealthConnect.class})
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 	@Mapping(target = "oauthId", source = "oAuthInfoResponse.oauthId")
 	@Mapping(target = "oauthProvider", source = "oAuthInfoResponse.oauthProvider")
