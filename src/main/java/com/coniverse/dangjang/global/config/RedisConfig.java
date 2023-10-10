321c8c626772d2751708b4d97e51ea1adb478906
13wjdgk <13wjdgk@naver.com>
2023-10-11 01:47:11 +0900

    chore : Embedded redis 추가

--- src/main/java/com/coniverse/dangjang/global/config/RedisConfig.java
+++ src/main/java/com/coniverse/dangjang/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.coniverse.dangjang.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
  * @author EVE
  * @since 1.0.0
  */
+@Profile("!test")
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
