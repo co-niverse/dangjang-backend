4cc2161b529c0eaa1ce6c2b8fb003172547c143b
NAM GIBEOM <rlqja0523@naver.com>
2023-10-26 00:43:21 +0900

    feat: app 버전 업데이트

--- src/main/java/com/coniverse/dangjang/domain/intro/dto/Version.java
+++ src/main/java/com/coniverse/dangjang/domain/intro/dto/Version.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 
 @AllArgsConstructor
 public enum Version {
-	MINIMUM("1.0.5"),
-	LATEST("1.0.5");
+	MINIMUM("1.0.7"),
+	LATEST("1.0.7");
 
 	@Getter
 	private final String version;
