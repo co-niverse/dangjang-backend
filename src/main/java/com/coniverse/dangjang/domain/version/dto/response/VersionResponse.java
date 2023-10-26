db1c7ead10c79f859ed54afa718217eb8bc4d853
NAM GIBEOM <rlqja0523@naver.com>
2023-10-26 16:50:23 +0900

    feat: 생성자 구현

--- src/main/java/com/coniverse/dangjang/domain/version/dto/response/VersionResponse.java
+++ src/main/java/com/coniverse/dangjang/domain/version/dto/response/VersionResponse.java
@@ -2,13 +2,17 @@ package com.coniverse.dangjang.domain.version.dto.response;
 
 import java.util.List;
 
+import com.coniverse.dangjang.domain.version.entity.Version;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * 버전 정보를 반환하는 dto이다.
  *
  * @author TEO
- * @since 1.0.0
+ * @since 1.3.0
  */
 public record VersionResponse<T>(String minVersion, String latestVersion, @JsonInclude(JsonInclude.Include.NON_NULL) List<T> loadData) {
+	public VersionResponse(Version version, List<T> loadData) {
+		this(version.getMinVersion(), version.getLatestVersion(), loadData);
+	}
 }
