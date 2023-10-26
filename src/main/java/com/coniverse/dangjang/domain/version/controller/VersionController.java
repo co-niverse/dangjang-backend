1a9e10d9542f0777e09a58832aecd65e0543ad9d
NAM GIBEOM <rlqja0523@naver.com>
2023-10-26 17:15:13 +0900

    feat: 앱 버전을 저장하는 기능 추가

--- src/main/java/com/coniverse/dangjang/domain/version/controller/VersionController.java
+++ src/main/java/com/coniverse/dangjang/domain/version/controller/VersionController.java
@@ -3,13 +3,17 @@ package com.coniverse.dangjang.domain.version.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coniverse.dangjang.domain.version.dto.request.VersionRequest;
 import com.coniverse.dangjang.domain.version.dto.response.VersionResponse;
 import com.coniverse.dangjang.domain.version.service.VersionService;
 import com.coniverse.dangjang.global.dto.SuccessSingleResponse;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -32,6 +36,17 @@ public class VersionController {
 	@GetMapping("/intro")
 	public ResponseEntity<SuccessSingleResponse<VersionResponse<?>>> getIntro() {
 		VersionResponse<?> versionResponse = versionService.getVersionResponse();
+		return ResponseEntity.ok().body(new SuccessSingleResponse<>(HttpStatus.OK.getReasonPhrase(), versionResponse));
+	}
+
+	/**
+	 * 버전 정보를 POST 요청한다.
+	 *
+	 * @since 1.3.0
+	 */
+	@PostMapping
+	public ResponseEntity<SuccessSingleResponse<VersionResponse<?>>> postVersion(@Valid @RequestBody VersionRequest request) {
+		VersionResponse<?> versionResponse = versionService.saveVersion(request);
 		return ResponseEntity.ok().body(new SuccessSingleResponse<>(HttpStatus.OK.getReasonPhrase(), versionResponse));
 	}
 }
