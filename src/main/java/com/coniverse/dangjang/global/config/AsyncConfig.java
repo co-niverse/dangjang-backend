8d470bcfd7d402cbbf1f3d35665cb4f733ba79bc
NAM GIBEOM <rlqja0523@naver.com>
2023-11-11 23:20:15 +0900

    refactor: async 설정 파일 분리

---
+++ src/main/java/com/coniverse/dangjang/global/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.coniverse.dangjang.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig { // TODO executor 설정
+}
